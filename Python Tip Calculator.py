231320dd07533e4df416421e90dd04e463e44a48
Blake Calhoun <blakecalhoun35@gmail.com>
2017-11-13 05:29:03 -0600

    Add files via upload

--- Python Tip Calculator.py
+++ Python Tip Calculator.py
@@ -1,8 +1,15 @@
+#Tip Calculator for all 50 States plus Washington D.C.
+print "Select your state"
+state = raw_input()
+
 def tax(bill):
   """Adds 8% tax to a restaurant bill."""
   bill *= 1.08
   print "With tax: %f" % bill
   return bill
+
+    if alabama
+        return alabama_tax_rate
 
 def tip(bill):
   """Adds 15% tip to a restaurant bill."""
@@ -10,6 +17,57 @@ def tip(bill):
   print "With tip: %f" % bill
   return bill
 
-meal_cost = 100
+#Meal variables
+meal_cost = raw_input()
 meal_with_tax = tax(meal_cost)
 meal_with_tip = tip(meal_with_tax)
+
+#Dictionary
+state_tax_rate = {'Alabama': 4/100, 'Alaska': 0, 'Arizona': 5.6/100, 'Arkansas': 6.5/100, 'California': 7.25/100, 'Colorado': 2.90/100}
+
+# 2017 State tax rates
+connecticut_tax_rate = 6.35/ 100
+delaware_tax_rate =	0.00
+florida_tax_rate =6.00
+Georgia	4.00%
+Hawaii (c)	4.00%
+Idaho	6.00%
+Illinois	6.25%
+Indiana	7.00%
+Iowa	6.00%
+Kansas	6.50%
+Kentucky	6.00%
+Louisiana	5.00%
+Maine	5.50%
+Maryland	6.00%
+Massachusetts	6.25%
+Michigan	6.00%
+Minnesota	6.88%
+Mississippi	7.00%
+Missouri	4.23%
+Montana (d)	0.00%
+Nebraska	5.50%
+Nevada	6.85%
+New Hampshire	0.00%
+New Jersey (e)	6.88%
+New Mexico (c)	5.13%
+New York	4.00%
+North Carolina	4.75%
+North Dakota	5.00%
+Ohio	5.75%
+Oklahoma	4.50%
+Oregon	0.00%
+Pennsylvania	6.00%
+Rhode Island	7.00%
+South Carolina	6.00%
+South Dakota (c)	4.50%
+Tennessee	7.00%
+Texas	6.25%
+Utah (b)	5.95%
+Vermont	6.00%
+Virginia (b)	5.30%
+Washington	6.50%
+West Virginia	6.00%
+Wisconsin	5.00%
+Wyoming	4.00%
+D.C.	5.75%
